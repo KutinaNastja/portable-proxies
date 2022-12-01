34dc80dbfd67acffcadfba09ed5f8325df3546d7
Anton <byghoster@gmail.com>
2022-12-01 17:59:58 +0300

    init

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,7 +3,9 @@ import styles from "./Button.module.scss";
 export const Button = () => {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.background}>ЗАКАЗАТЬ</div>
+      <div className={styles.background}>
+        <a href="https://t.me/@antonkb">ЗАКАЗАТЬ</a>
+      </div>
     </div>
   );
 };
--- src/components/ButtonSmall/ButtonSmall.tsx
+++ src/components/ButtonSmall/ButtonSmall.tsx
@@ -3,7 +3,9 @@ import styles from "./ButtonSmall.module.scss";
 export const ButtonSmall = () => {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.background}>ЗАКАЗАТЬ</div>
+      <div className={styles.background}>
+        <a href="https://t.me/@antonkb">ЗАКАЗАТЬ</a>
+      </div>
     </div>
   );
 };
